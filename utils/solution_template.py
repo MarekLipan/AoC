44be619af99c7238478638acffada8bdb82bd98d
Marek.Lipan <marek.lipan.ml@gmail.com>
2023-07-30 15:30:10 +0200

    Update solution template

--- utils/solution_template.py
+++ utils/solution_template.py
@@ -6,7 +6,7 @@ import os
 from typing import List
 
 
-def solution(inp: List(str)):
+def solution(inp: List[str]):
     """
     Solution for the day's puzzle
     """
@@ -20,15 +20,24 @@ def main():
     """
     Function to solve the puzzle
     """
+
     with open(
-        os.path.join(os.getcwd(), "test_input.txt"), "r", encoding="utf-8"
+        os.path.join(os.path.realpath(__file__), "../test_input.txt"),
+        "r",
+        encoding="utf-8",
     ) as input_file:
         task_test_input = input_file.readlines()
 
     with open(
-        os.path.join(os.getcwd(), "input.txt"), "r", encoding="utf-8"
+        os.path.join(os.path.realpath(__file__), "../input.txt"),
+        "r",
+        encoding="utf-8",
     ) as input_file:
         task_input = input_file.readlines()
 
     print(f"Test input Solution: {solution(task_test_input)}")
     print(f"Solution: {solution(task_input)}")
+
+
+if __name__ == "__main__":
+    main()
